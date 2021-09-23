93c107bd79db38d0229dbec7a1acabea60669080
Kaj Białas <kaj.bialas@dev-industry.com>
2021-09-23 13:33:27 +0200

    issue

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const http = require('http');
+const moment = require('moment');
 
 const HOSTNAME = '127.0.0.1';
 const PORT = process.env.PORT || 3000;
-console.log('proces', process.env);
+
+console.log(moment().format('dddd'));
 
 const server = http.createServer((request, response) => {
     response.statusCode = 200;
     response.setHeader('Content-Type', 'text/html');
-    response.end('Hello World');
+    response.end(moment().format('dddd'));
 });
 
 server.listen(PORT, HOSTNAME, () => {
