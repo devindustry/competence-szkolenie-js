7190074bf76e74748ba8f402e610cef14a00f895
Kaj Białas <kaj.bialas@dev-industry.com>
2021-09-30 10:21:00 +0200

    useEffect

--- src/pages/PostDetails.js
+++ src/pages/PostDetails.js
@@ -1,33 +1,47 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import Comments from "../components/Comments";
 const API_URL = 'https://jsonplaceholder.typicode.com/posts/';
 
-class PostDetails extends Component {
-    state = {
-        postDetails: {},
-    };
+const PostDetails = (props) => {
+    const [ postDetails, setPostDetails ] = useState({});
+    const [ loading, setLoading ] = useState(false);
+    const [ error, setError ] = useState(false);
 
-    componentDidMount() {
-        fetch(`${API_URL}${this.props.match.params.id}`)
+    useEffect(() => {
+        setLoading(true);
+        fetch(`${API_URL}${props.match.params.id}`)
             .then(response => {
+                if (response.status > 400) {
+                    throw new Error();
+                }
                 return response.json();
             })
-            .then(data => setTimeout(() => this.setState({
-                postDetails: data,
-            }), 200));
+            .then(data => setTimeout(() => {
+                    setPostDetails(data);
+                    setLoading(false);
+                }, 200)
+            )
+            .catch(e => {
+                setError(true);
+                setLoading(false);
+            });
+    }, []);
+
+    if (loading) {
+        return (<div>Ładowanie...</div>);
     }
 
-    render() {
-        return (
-            <div>
-                <h1>Szczegóły postu</h1>
-                <h3>{this.state.postDetails.title}</h3>
-                <p>{this.state.postDetails.body}</p>
-                <Comments />
-            </div>
-        )
+    if (error) {
+        return (<div>Błąd pobierania danych...</div>)
     }
-
+    return (
+        <div>
+            <h1>Szczegóły postu</h1>
+            <h3>{postDetails.title}</h3>
+            <p>{postDetails.body}</p>
+            <Comments />
+        </div>
+    )
 }
 
 export default PostDetails;
